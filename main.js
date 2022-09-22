63e1ced675cb8b3c7c0b60fd17695160dd60afc0
unohayato <mamidori1p1p@gmail.com>
2022-09-22 15:46:08 +0900

    done

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-let simplemde = new SimpleMDE({ element: document.getElementById("editor") });
+let simplemde = new SimpleMDE({ 
+  element: document.getElementById("editor"), 
+  forceSync: true,
+  spellChecker: false,
+  showIcons: ["code", "table"],
+ });
+
+const markdown = document.getElementById("editor");
+
+function drawPreview() {
+  const html = SimpleMDE.prototype.markdown(simplemde.value());
+  document.getElementById("preview").innerHTML = html;
+};
+
+document.addEventListener("input", () => {
+  drawPreview();
+});
